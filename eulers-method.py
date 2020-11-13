ebfd46aa7291921d442bdb1991a6f773a33d6033
sekharm705 <34575076+sekharm705@users.noreply.github.com>
2020-11-13 20:38:14 +0530

    Create eulers-method.py

---
+++ eulers-method.py
@@ -0,0 +1,50 @@
+#Calculated for theta = 0.001, 0.01, 0.1, 0.5, 1, 1.5, 2, 2.5, 3, pi-0.001
+
+
+#package imports for graphing and computing sine
+import matplotlib.pyplot as plt
+import math
+
+#constants
+gravity = 10
+length = 0.1
+step = 0.000001
+initialAngle = math.pi - 0.1
+angularVelocity = 0 #initial condition for angular velocity
+
+GbyL = gravity/length #computed once and saved instead of computing each time
+
+time = 0 #initial time set to zero
+theta = initialAngle #initialize the angle to the original angle
+
+previousValues = [theta+1, theta-1] #initialize this with arbitrary values that don't trigger the check, logic explained on line 27
+
+angles = [] #an array to store the angles to graph later
+
+while 1: #loop until we reach a maximum, after which we break out of the loop
+
+    acceleration = -GbyL * math.sin(theta) #calculating the acceleration
+    time += step
+    angularVelocity += step * acceleration
+    theta += angularVelocity*step
+
+    #logic to check whether it has reached a maximum: in loop n, if the (n-1)th value is greater than the (n-2)th value and greater than the nth value, it is a maximum and we can quit the calculation without saving this round.
+    if previousValues[0] < previousValues[1] and previousValues[1] > theta and time > 0.01:
+        break
+    previousValues[0] = previousValues[1]
+    previousValues[1] = theta
+
+    angles.append(theta) #if we're still in the first oscillation, keep the value for graphing
+
+xValues = [i*step for i in range(len(angles))] #Creates an array of timestep values in order, with the same length as the array of angles.
+
+plt.scatter(xValues, angles, color='#000000', marker="o", s=0.1) #scatterplot the time and angle.
+
+linearPrediction = [initialAngle*math.cos(i * math.sqrt(GbyL)) for i in xValues] #create an array of what the linear approximation (SHM pendulum) predicts.
+
+plt.scatter(xValues, linearPrediction, color='#0000FF', marker="o", s=0.1) #scatterplot the time and prediction.
+plt.title(r"$\theta_0 = " + str(initialAngle) + r"$") #mark the title
+approximatePeriod = 2*math.pi*math.sqrt(length/gravity) #calculate the approximate period
+percentageError = str(round(100*((time - approximatePeriod)/approximatePeriod), 3)) #calculate the error
+plt.xlabel("Real period = "+ str(round(time, 5)) + ", Percentage error = "+percentageError+ "%") #mark the error, period
+plt.show() #show the graph
