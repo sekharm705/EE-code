8fb6191cbeb7515be6d8bf6da2c1f59306436387
Sekhar Musunuri <34575076+sekharm705@users.noreply.github.com>
2021-02-19 19:06:33 +0530

    minor modifications for speed

--- eulers-method.py
+++ eulers-method.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt # matplotlib must be installed, NumPy >= 1.15 al
 import math
 
 #constants
-gravity = 10
-length = 0.1
-step = 0.000001
-initialAngle = <initial angle>
+gravity = 10 #value of gravity - 10 m/s^2
+length = 0.1 #length of the pendulum - from pivot point to the point bob.
+step = 0.000001 #smaller value makes the calculation more accurate, but increases program time. 
+initialAngle = float(input("Enter the initial angle: "))
 angularVelocity = 0 #initial condition for angular velocity
 
 GbyL = gravity/length #computed once and saved instead of computing each time
@@ -37,12 +37,12 @@ while 1: #loop until we reach a maximum, after which we break out of the loop
     angles.append(theta) #if we're still in the first oscillation, keep the value for graphing
 
 xValues = [i*step for i in range(len(angles))] #Creates an array of timestep values in order, with the same length as the array of angles.
+plt.scatter(xValues[::100], angles[::100], color='#000000', marker="o", s=0.1) #scatterplot the time and angle. This is the slowest part of the code. Plotting 1 in every 100 values is a good balance between speed and smoothness of the curve
 
-plt.scatter(xValues, angles, color='#000000', marker="o", s=0.1) #scatterplot the time and angle.
+singlePeriod = [i*step for i in range(int(length * math.pi * (2/step)))]
+linearPrediction = [initialAngle*math.cos(i * math.sqrt(GbyL)) for i in singlePeriod] #create an array of what one oscillation of the linear approximation (SHM pendulum) predicts.
 
-linearPrediction = [initialAngle*math.cos(i * math.sqrt(GbyL)) for i in xValues] #create an array of what the linear approximation (SHM pendulum) predicts.
-
-plt.scatter(xValues, linearPrediction, color='#0000FF', marker="o", s=0.1) #scatterplot the time and prediction.
+plt.scatter(singlePeriod[::100], linearPrediction[::100], color='#0000FF', marker="o", s=0.1) #scatterplot the time and prediction.
 approximatePeriod = 2*math.pi*math.sqrt(length/gravity) #calculate the approximate period
 plt.title(r"$\theta_0 = " + str(initialAngle) + r"$" + "\n Real period = "+ str(round(time, 7)) + ", Approximated period = " + str(round(approximatePeriod, 7))) #mark the title
 plt.xlabel("Time") #Label the X axis
